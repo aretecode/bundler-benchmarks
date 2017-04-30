9087ea5c7de91c89968db7e1301cc0a45eaff20b
Arete Code <aretecode@gmail.com>
2017-04-30 03:17:55 -0700

    🏋️📇📜 update pkg, bench data, &✚ require bench

---
+++ bench-require-dynamic.js
@@ -0,0 +1,28 @@
+const {fus, web, roll} = require('./configs')
+const Record = require('bench-chain')
+
+const name = 'dynamicrequire'
+const record = Record.init(__dirname, `./results-${name}.json`)
+
+web(name)
+// roll(name)
+record
+  .tags('metal')
+  .addAsync('fusebox - ' + name, async done => {
+    const eh = await fus(name)
+    done(eh)
+    return eh
+  })
+  // .addAsync('rollup - ' + name, async done => {
+  //   const eh = await roll(name)
+  //   done(eh)
+  //   return eh
+  // })
+  // .addAsync('webpack - ' + name, async done => {
+  //   const eh = await web(name)
+  //   done(eh)
+  //   return eh
+  // })
+  .run()
+
+// record.setup().echo()
---
+++ bench-require.js
@@ -0,0 +1,1 @@
+require('./config-bench-factory')('required')
